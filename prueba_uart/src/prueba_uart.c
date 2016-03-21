c04efdb948e212e9404d2d4f59a5b5126f71c25a
M3gabit <juan.dastugue@gmail.com>
2016-03-21 15:11:37 -0300

    Agregada funcion de los leds

--- prueba_uart/src/prueba_uart.c
+++ prueba_uart/src/prueba_uart.c
@@ -27,7 +27,7 @@ static uint8_t num3 = 0;
 static uint8_t num4 = 0;
 
 
-static uint16_t hr_ciaaDac;
+static uint16_t hr_ciaaAdc;
 static uint16_t aux;
 
 
@@ -57,12 +57,10 @@ TASK(InitTask)
 
    /* open UART connected to USB bridge (FT2232) */
    fd_uart1 = ciaaPOSIX_open("/dev/serial/uart/1", ciaaPOSIX_O_RDWR);
-
    ciaaPOSIX_ioctl(fd_uart1, ciaaPOSIX_IOCTL_SET_BAUDRATE, (void *)ciaaBAUDRATE_115200);
-
    ciaaPOSIX_ioctl(fd_uart1, ciaaPOSIX_IOCTL_SET_FIFO_TRIGGER_LEVEL, (void *)ciaaFIFO_TRIGGER_LEVEL3);
 
-
+   // open ADC 
    fd_adc = ciaaPOSIX_open("/dev/serial/aio/in/0", ciaaPOSIX_O_RDONLY);
    ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_SAMPLE_RATE, 100000);
    ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_3);
@@ -75,7 +73,7 @@ TASK(InitTask)
    //Activa la tarea PeriodicTask cada 200 ticks
    SetRelAlarm(ActivatePeriodicTask, 100, 100);
 
-   //ActivateTask(SerialEchoTask);
+   ActivateTask(SerialEchoTask);
 
    /* end InitTask */
    TerminateTask();
@@ -83,67 +81,112 @@ TASK(InitTask)
 
 TASK(SerialEchoTask)
 {
-   char buf[20];   /* buffer for uart operation              */
-   uint8_t outputs;  /* to store outputs status                */
-   int32_t ret;      /* return value variable for posix calls  */
-   char message[] = "Hi! :)\nSerialechotask: waiting for characters...\n";
+    char buf[20];   /* buffer for uart operation              */
+    uint8_t outputs;  /* to store outputs status                */
+    int32_t ret;      /* return value variable for posix calls  */
+	char lr[] = "led_r\0";
+	char l1on[] = "led_1_on\0";
+	char l2on[] = "led_2_on\0";
+	char l3on[] = "led_3_on\0";
+	char l1off[] = "led_1_off\0";
+	char l2off[] = "led_2_off\0";
+	char l3off[] = "led_3_off\0";
+	
 
+   //char message[] = "Hi! :)\nSerialechotask: waiting for characters...\n";
 
    while(1)
    {
-   
-   }
+   		ret = ciaaPOSIX_read(fd_uart1, buf, 20);
+
+
+   		if(ciaaPOSIX_strcmp(buf,l1on) == 0)
+      {
+         ciaaPOSIX_read(fd_out, &outputs, 1);
+         outputs |= 0x08;
+         ciaaPOSIX_write(fd_out, &outputs, 1);
+      }
+
+      if(ciaaPOSIX_strcmp(buf,l1off) == 0)
+      {
+         ciaaPOSIX_read(fd_out, &outputs, 1);
+         outputs &= 0xf7;
+         ciaaPOSIX_write(fd_out, &outputs, 1);
+      }
+
+   		if(ciaaPOSIX_strcmp(buf,l2on) == 0)
+      	{
+         ciaaPOSIX_read(fd_out, &outputs, 1);
+         outputs |= 0x10;
+         ciaaPOSIX_write(fd_out, &outputs, 1);
+     	}
+
+     	if(ciaaPOSIX_strcmp(buf,l2off) == 0)
+      	{
+         ciaaPOSIX_read(fd_out, &outputs, 1);
+         outputs &= 0xef;
+         ciaaPOSIX_write(fd_out, &outputs, 1);
+     	}
+
+     	if(ciaaPOSIX_strcmp(buf,l3on) == 0)
+      {
+          ciaaPOSIX_read(fd_out, &outputs, 1);
+          outputs |= 0x20;
+          ciaaPOSIX_write(fd_out, &outputs, 1);
+      }
+
+      if(ciaaPOSIX_strcmp(buf,l3off) == 0)
+      {
+          ciaaPOSIX_read(fd_out, &outputs, 1);
+          outputs &= 0xdf;
+          ciaaPOSIX_write(fd_out, &outputs, 1);
+      }
+
+     	
+    }
 }
 
 
 TASK(PeriodicTask)
 {
-    uint8_t outputs = 0;
-    aux=hr_ciaaDac;
+	uint8_t outputs = 0;
+	aux=hr_ciaaAdc;
 
 
-    x1 = (hr_ciaaDac/1000);
-    num1 = x1+48;
+	x1 = (hr_ciaaAdc/1000);
+	num1 = x1+48;
 
-	x2=((hr_ciaaDac-x1*1000)/100);
+	x2=((hr_ciaaAdc-x1*1000)/100);
 	num2=x2+48;
 
-	x3=((hr_ciaaDac-x1*1000-x2*100)/10);
+	x3=((hr_ciaaAdc-x1*1000-x2*100)/10);
 	num3=x3+48;
 
-	x4=(hr_ciaaDac-x1*1000-x2*100-x3*10);
+	x4=(hr_ciaaAdc-x1*1000-x2*100-x3*10);
 	num4=x4+48;
 
 	char dato[] = {num1,num2,num3,num4};
 	//Esto funciona
    
    // char message[] = "\n";
-	//ciaaPOSIX_write(fd_uart1, &hr_ciaaDac, sizeof(hr_ciaaDac));
+	//ciaaPOSIX_write(fd_uart1, &hr_ciaaAdc, sizeof(hr_ciaaAdc));
 	ciaaPOSIX_write(fd_uart1, dato, ciaaPOSIX_strlen(dato));
 
-	//myvar++;
-   ciaaPOSIX_read(fd_out, &outputs, 1);
-   outputs ^= 0x04;
-   ciaaPOSIX_write(fd_out, &outputs, 1);
-   //if (myvar > 100)
-   //{
-   //	myvar = 0;
-   //}
-   
+	ciaaPOSIX_read(fd_out, &outputs, 1);
+	outputs ^= 0x04;
+	ciaaPOSIX_write(fd_out, &outputs, 1);
 
-   TerminateTask();
+	TerminateTask();
 }
 
 
 TASK(Analogic)
 {
-   
-
    /* Read ADC. */
-   ciaaPOSIX_read(fd_adc, &hr_ciaaDac, sizeof(hr_ciaaDac));
+   ciaaPOSIX_read(fd_adc, &hr_ciaaAdc, sizeof(hr_ciaaAdc));
 
    /* Signal gain. */
-   hr_ciaaDac >>= 0;
+   hr_ciaaAdc >>= 0;
 
    /* end of Blinking */
    TerminateTask();
