3f20906ac8e04befcec4222a41b6d9eafbdf5b51
M3gabit <juan.dastugue@gmail.com>
2016-12-08 09:14:21 -0300

    Agregado tareas analogicas

--- prueba_uart/src/prueba_uart.c
+++ prueba_uart/src/prueba_uart.c
@@ -1,4 +1,3 @@
-
 #include "os.h"               /* <= operating system header */
 #include "ciaaPOSIX_stdio.h"  /* <= device handler header */
 #include "ciaaPOSIX_string.h" /* <= string header */
@@ -16,6 +15,14 @@ static int32_t fd_uart1;
 
 static uint32_t Periodic_Task_Counter;
 
+static int habilitar = 0;
+static int anlogOn = 0;
+static int ch1 = 0;
+static int ch2 = 0;
+
+static uint8_t aux_adc = 0;
+static uint8_t var_aux = 0;
+
 static uint8_t x1 = 0;
 static uint8_t x2 = 0;
 static uint8_t x3 = 0;
@@ -26,19 +33,21 @@ static uint8_t num2 = 0;
 static uint8_t num3 = 0;
 static uint8_t num4 = 0;
 
+static uint8_t y1 = 0;
+static uint8_t y2 = 0;
+static uint8_t y3 = 0;
+static uint8_t y4 = 0;
 
-static uint16_t hr_ciaaAdc;
-static uint16_t hr_ciaaAdc2;
-static uint8_t aux_adc = 0;
-static uint8_t var_aux = 0;
-
+static uint8_t nu1 = 0;
+static uint8_t nu2 = 0;
+static uint8_t nu3 = 0;
+static uint8_t nu4 = 0;
 
 int main(void)
 {
    StartOS(AppMode1);
    return 0;
 }
-
 
 void ErrorHook(void)
 {
@@ -65,17 +74,19 @@ TASK(InitTask)
    // open ADC ch3
    fd_adc = ciaaPOSIX_open("/dev/serial/aio/in/0", ciaaPOSIX_O_RDONLY);
    ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_SAMPLE_RATE, 100000);
-   ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_3);
+   ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_1);
 
-   ActivateTask(Analogic);
-
+   //ActivateTask(Analogic);
 
    /* activate example tasks */
    Periodic_Task_Counter = 0;
    //Activa la tarea PeriodicTask cada 200 ticks
-   SetRelAlarm(ActivatePeriodicTask, 100, 100);
+   //SetRelAlarm(ActivatePeriodicTask, 100, 100);
 
    ActivateTask(SerialEchoTask);
+   //SetRelAlarm(AnalogicoUno, 100,100);
+   // SetRelAlarm(AnalogicoDos, 120,100);
+   // SetRelAlarm(AnalogicoTres, 140,100);
 
    /* end InitTask */
    TerminateTask();
@@ -83,76 +94,169 @@ TASK(InitTask)
 
 TASK(SerialEchoTask)
 {
-    char buf[20];   /* buffer for uart operation              */
-    uint8_t outputs;  /* to store outputs status                */
-    int32_t ret;      /* return value variable for posix calls  */
-	char lr[] = "led_r\0";
-	char l1on[] = "led_1_on\0";
-	char l2on[] = "led_2_on\0";
-	char l3on[] = "led_3_on\0";
-	char l1off[] = "led_1_off\0";
-	char l2off[] = "led_2_off\0";
-	char l3off[] = "led_3_off\0";
-	
+  char buf[20];   /* buffer for uart operation              */
+  char buf2[20];
+  uint32_t outputs;  /* to store outputs status                */
+  int32_t ret;      /* return value variable for posix calls  */
+  char iniciar[] = "iniciar\0";
+  char handshake[] = "123ok\r\n";
+	char desconec[] = "desconectar\0";
 
-   //char message[] = "Hi! :)\nSerialechotask: waiting for characters...\n";
+	char analogico_on[] = "an_on\0";
+	char analogico_off[] = "an_off\0";
+
+	char analog1_on[] = "ch1_on\0";
+	char analog1_off[] = "ch1_off\0";
+
+	char analog2_on[] = "ch2_on\0";
+	char analog2_off[] = "ch2_off\0";
+
+	char analog3_on[] = "ch3_on\0";
+	char analog3_off[] = "ch3_off\0";
+
+	static int alarm1 = 0;
+	static int alarm2 = 0;
+	static int alarm3 = 0;
+	static int var = 0;
 
    while(1)
    {
    		ret = ciaaPOSIX_read(fd_uart1, buf, 20);
 
+   		if (ciaaPOSIX_strcmp(buf,iniciar) == 0)
+   		{
+   			ciaaPOSIX_write(fd_uart1, handshake, ciaaPOSIX_strlen(handshake));
+   			SetRelAlarm(Handshake, 100, 100);
+   			//PONE HABILITAR EN 1
+   		}
 
-   		if(ciaaPOSIX_strcmp(buf,l1on) == 0)
-      {
-         ciaaPOSIX_read(fd_out, &outputs, 1);
-         outputs |= 0x08;
-         ciaaPOSIX_write(fd_out, &outputs, 1);
-      }
+   		if (habilitar == 1)
+   		{
+   			if(ciaaPOSIX_strcmp(buf,analog1_on) == 0)
+      		{
+      			ret = ciaaPOSIX_read(fd_uart1, buf2, 20);
 
-      if(ciaaPOSIX_strcmp(buf,l1off) == 0)
-      {
-         ciaaPOSIX_read(fd_out, &outputs, 1);
-         outputs &= 0xf7;
-         ciaaPOSIX_write(fd_out, &outputs, 1);
-      }
+  				  var = ((buf2[0]-48)*1000) + ((buf2[1]-48)*100) + (buf2[2]-48)*10 + buf2[3]-48;
+         		SetRelAlarm(AnalogicoUno, var,var);
+         		alarm1=1;
+      		}
 
-   		if(ciaaPOSIX_strcmp(buf,l2on) == 0)
-      	{
-         ciaaPOSIX_read(fd_out, &outputs, 1);
-         outputs |= 0x10;
-         ciaaPOSIX_write(fd_out, &outputs, 1);
-     	}
+      		if(ciaaPOSIX_strcmp(buf,analog1_off) == 0)
+      		{
+	         	alarm1=0;
+	         	CancelAlarm(AnalogicoUno);
+	         	ciaaPOSIX_read(fd_out, &outputs, 2);
+				outputs &= 0xfff7;//Apago el led 1
+				ciaaPOSIX_write(fd_out, &outputs, 2);
+      		}
 
-     	if(ciaaPOSIX_strcmp(buf,l2off) == 0)
-      	{
-         ciaaPOSIX_read(fd_out, &outputs, 1);
-         outputs &= 0xef;
-         ciaaPOSIX_write(fd_out, &outputs, 1);
-     	}
+      		if(ciaaPOSIX_strcmp(buf,analog2_on) == 0)
+      		{
+      			// ret = ciaaPOSIX_read(fd_uart1, buf2, 20);
 
-     	if(ciaaPOSIX_strcmp(buf,l3on) == 0)
-      {
-          ciaaPOSIX_read(fd_out, &outputs, 1);
-          outputs |= 0x20;
-          ciaaPOSIX_write(fd_out, &outputs, 1);
-      }
+  				// var = ((buf2[0]-48)*1000) + ((buf2[1]-48)*100) + (buf2[2]-48)*10 + buf2[3]-48;
+      			SetRelAlarm(AnalogicoDos, 100, 100);
+      			alarm2=1;
+      		}
 
-      if(ciaaPOSIX_strcmp(buf,l3off) == 0)
-      {
-          ciaaPOSIX_read(fd_out, &outputs, 1);
-          outputs &= 0xdf;
-          ciaaPOSIX_write(fd_out, &outputs, 1);
-      }
+      		if(ciaaPOSIX_strcmp(buf,analog2_off) == 0)
+      		{
+         		alarm2=0;
+         		CancelAlarm(AnalogicoDos);
+         		ciaaPOSIX_read(fd_out, &outputs, 2);
+				outputs &= 0xfffb;//Apago el led 2
+				ciaaPOSIX_write(fd_out, &outputs, 2);
+      		}
 
-     	
+      		if(ciaaPOSIX_strcmp(buf,analog3_on) == 0)
+      		{
+
+      			// ret = ciaaPOSIX_read(fd_uart1, buf2, 20);
+
+  				// var = ((buf2[0]-48)*1000) + ((buf2[1]-48)*100) + (buf2[2]-48)*10 + buf2[3]-48;
+      			SetRelAlarm(AnalogicoTres, 100, 100);
+      			alarm3=1;
+      		}
+
+      		if(ciaaPOSIX_strcmp(buf,analog3_off) == 0)
+      		{
+         		alarm3=0;
+         		CancelAlarm(AnalogicoTres);
+         		ciaaPOSIX_read(fd_out, &outputs, 2);
+				    outputs &= 0xfffd;//Apago el led 3
+				    ciaaPOSIX_write(fd_out, &outputs, 2);
+      		}
+
+      		if (ciaaPOSIX_strcmp(buf,desconec) == 0)
+      		{
+      			habilitar =0;
+      			if(alarm1==1)
+      			{
+	      			CancelAlarm(AnalogicoUno);
+	      			alarm1=0;
+      			}
+
+      			if(alarm2==1)
+      			{
+	      			CancelAlarm(AnalogicoDos);
+	      			alarm2=0;
+      			}
+
+      			if(alarm3==1)
+      			{
+	      			CancelAlarm(AnalogicoTres);
+	      			alarm3=0;
+      			}
+      			ciaaPOSIX_read(fd_out, &outputs, 2);
+				    outputs &= 0xffdf;//Apago el led verde
+				    ciaaPOSIX_write(fd_out, &outputs, 2);
+      			ciaaPOSIX_read(fd_out, &outputs, 2);
+				    outputs &= 0xfff7;//Apago el led 1
+				    ciaaPOSIX_write(fd_out, &outputs, 2);
+      			ciaaPOSIX_read(fd_out, &outputs, 2);
+				    outputs &= 0xfffb;//Apago el led 2
+				    ciaaPOSIX_write(fd_out, &outputs, 2);
+      			ciaaPOSIX_read(fd_out, &outputs, 2);
+				    outputs &= 0xfffd;//Apago el led 3
+				    ciaaPOSIX_write(fd_out, &outputs, 2);
+      		}
+
+   		}
     }
 }
 
+TASK(Leds) //Handshake
+{
+	static int counter = 0;
+	uint32_t outputs;
+
+	ciaaPOSIX_read(fd_out, &outputs, 2);
+   	outputs ^= 0x0400;
+   	ciaaPOSIX_write(fd_out, &outputs, 2);
+
+	if (counter ++ >= 20)
+	{
+		CancelAlarm(Handshake);
+		counter=0;
+		habilitar= 1;
+		ciaaPOSIX_read(fd_out, &outputs, 1);
+        outputs |= 0x02;
+        ciaaPOSIX_write(fd_out, &outputs, 1);
+	}
+   	TerminateTask();
+}
 
 TASK(PeriodicTask)
 {
-	uint8_t outputs = 0;
-	
+	//No se activa nunca
+}
+
+TASK(AnalogicUno)
+{
+  uint32_t outputs = 0;
+  uint16_t hr_ciaaAdc;
+
+	ciaaPOSIX_read(fd_adc, &hr_ciaaAdc, sizeof(hr_ciaaAdc));
 
 	x1 = (hr_ciaaAdc/1000);
 	num1 = x1+48;
@@ -166,75 +270,77 @@ TASK(PeriodicTask)
 	x4=(hr_ciaaAdc-x1*1000-x2*100-x3*10);
 	num4=x4+48;
 
-	char dato[] = {'C',num1,num2,num3,num4};
-	//Esto funciona
+	char dato[] = {'A',num1,num2,num3,num4,'\r\n'};
 
-	x1 = (hr_ciaaAdc2/1000);
-	num1 = x1+48;
+	ciaaPOSIX_write(fd_uart1, dato, ciaaPOSIX_strlen(dato));
 
-	x2=((hr_ciaaAdc2-x1*1000)/100);
-	num2=x2+48;
+	ciaaPOSIX_read(fd_out, &outputs, 2);
+	outputs ^= 0x1000;
+	ciaaPOSIX_write(fd_out, &outputs, 2);
 
-	x3=((hr_ciaaAdc2-x1*1000-x2*100)/10);
-	num3=x3+48;
+	ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_1);
+   	TerminateTask();
+}
 
-	x4=(hr_ciaaAdc2-x1*1000-x2*100-x3*10);
-	num4=x4+48;
+TASK(AnalogicDos)
+{
+	uint32_t outputs;
+	uint16_t hr_ciaaAdc2;
 
-	char dato2[] = {'B',num1,num2,num3,num4};
+	ciaaPOSIX_read(fd_adc, &hr_ciaaAdc2, sizeof(hr_ciaaAdc2));//CH2
 
-   
-   // char message[] = "\n";
-	//ciaaPOSIX_write(fd_uart1, &hr_ciaaAdc, sizeof(hr_ciaaAdc));
+	y1 = (hr_ciaaAdc2/1000);
+	nu1 = y1+48;
 
-	if (var_aux == 0)
-	{
-		ciaaPOSIX_write(fd_uart1, dato, ciaaPOSIX_strlen(dato));
-		var_aux = 1;
-	}
-	else
-	{
-		ciaaPOSIX_write(fd_uart1, dato2, ciaaPOSIX_strlen(dato2));
-		var_aux = 0;
-	}
+	y2=((hr_ciaaAdc2-y1*1000)/100);
+	nu2=y2+48;
 
+	y3=((hr_ciaaAdc2-y1*1000-y2*100)/10);
+	nu3=y3+48;
 
-	
+	y4=(hr_ciaaAdc2-y1*1000-y2*100-y3*10);
+	nu4=y4+48;
 
-	ciaaPOSIX_read(fd_out, &outputs, 1);
-	outputs ^= 0x04;
-	ciaaPOSIX_write(fd_out, &outputs, 1);
+	char dato2[] = {'B',nu1,nu2,nu3,nu4,'\r\n'};
 
-	
+	ciaaPOSIX_write(fd_uart1, dato2, ciaaPOSIX_strlen(dato2));
 
+	ciaaPOSIX_read(fd_out, &outputs, 2);
+	outputs ^= 0x2000;
+	ciaaPOSIX_write(fd_out, &outputs, 2);
+
+	ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_2);
 	TerminateTask();
 }
 
-
-TASK(Analogic)
+TASK(AnalogicTres)
 {
-   /* Read ADC. */
-	if (aux_adc == 1)
-	{
-		ciaaPOSIX_read(fd_adc, &hr_ciaaAdc, sizeof(hr_ciaaAdc));//CH3
-		ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_2);
-    	
-    	aux_adc = 0;
-	}
-	else
-	{
-		ciaaPOSIX_read(fd_adc, &hr_ciaaAdc2, sizeof(hr_ciaaAdc2));//CH2
-		ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_3);
-		
-		aux_adc = 1;
-	}
+	uint16_t hr_ciaaAdc3;
+	uint32_t outputs;
 
+	ciaaPOSIX_read(fd_adc, &hr_ciaaAdc3, sizeof(hr_ciaaAdc3));//CH3
 
+	x1 = (hr_ciaaAdc3/1000);
+	num1 = x1+48;
 
+	x2=((hr_ciaaAdc3-x1*1000)/100);
+	num2=x2+48;
 
-   /* Signal gain. */
-   hr_ciaaAdc >>= 0;
+	x3=((hr_ciaaAdc3-x1*1000-x2*100)/10);
+	num3=x3+48;
 
-   /* end of Blinking */
-   TerminateTask();
+	x4=(hr_ciaaAdc3-x1*1000-x2*100-x3*10);
+	num4=x4+48;
+
+	char dato3[] = {'C',num1,num2,num3,num4,'\r\n'};
+
+	ciaaPOSIX_write(fd_uart1, dato3, ciaaPOSIX_strlen(dato3));
+
+	ciaaPOSIX_read(fd_out, &outputs, 2);
+	outputs ^= 0x4000;
+	ciaaPOSIX_write(fd_out, &outputs, 2);
+
+  ciaaPOSIX_ioctl(fd_adc, ciaaPOSIX_IOCTL_SET_CHANNEL, ciaaCHANNEL_1);
+
+	TerminateTask();
 }
